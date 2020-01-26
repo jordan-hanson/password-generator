984bab79469c89db15e7b2865c1fe2e28dae4e37
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-01-25 23:52:53 -0700

    started new file and updated my script functions

--- script.js
+++ script.js
@@ -1,90 +1,27 @@
-//DOM RESULT
-var resultEl = document.getElementById('result');
-var lengthEl = document.getElementById('length');
-var uppercaseEl = document.getElementById('uppercase');
-var lowercaseEl = document.getElementById('lowercase');
-var numberEl = document.getElementById('number');
-var symbolsEl = document.getElementById('symbols');
-var generateEl = document.getElementById('generate');
-var clipboardEl = document.getElementById('clipboard');
+var character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var number = '0123456789';
+var symbol = '~!@#$%^&*()<>?';
 
-var randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol,
-};
+var inputVal = document.getElementById('inputVal');
+var numCheck = document.getElementById('numCheck');
+var symCheck = document.getElementById('symCheck');
+var submitBtn = document.getElementById('submitBtn');
+var password = document.getElementById('password')
 
-clipboardEl.addEventListener('click', () => {
-    var textarea = document.createElement('textarea');
-    var password = resultEl.innerText;
+function password(length, characters) {
+    var pswrd = '';
+    for (var i = 0; i < length; i++) {
+        pswrd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pswrd;
+}
 
-    if (!password) { return; }
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    textarea.remove();
-    alert('Password copied to clipboard');
+submitBtn.addEventListener("click", function () {
+    var characters = character;
+    (numCheck.checked) ? characters += number : '';
+    (symCheck.checked) ? characters += symbol : '';
+    password.value = (inputVal.value, characters);
 });
 
 
-
-// Generate event : Listener
-generate.addEventListener('click', () => {
-    console.log('hey')
-
-    var length = +lengthEl.value;
-    var hasLower = lowercaseEl.checked;
-    var hasUpper = uppercaseEl.checked;
-    var hasNumber = numberEl.checked;
-    var hasSymbol = symbolsEl.checked;
-
-    console.log(hasLower, hasNumber, hasSymbol, hasUpper);
-
-    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-});
-// Generate password function
-function generatePassword(lower, upper, number, symbol, length) {
-    let generatedPassword = '';
-
-    var typesCount = lower + upper + number + symbol;
-
-    var typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
-        (
-            item => Object.values(item)[0]
-        );
-    if (typesCount === 0) {
-        return '';
-    }
-    for (let i = 0; i < length; i += typesCount) {
-        typesArr.forEach(type => {
-            var funcName = Object.keys(type)[0];
-
-            generatedPassword += randomFunc[funcName]();
-        });
-    }
-    var finalPassword = generatedPassword.slice(0, length);
-
-    return finalPassword;
-}
-
-// Generator Functions
-
-function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-
-}
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-
-}
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getRandomSymbol() {
-    var symbols = "!@#$%^&*(){}[]=;',./?"
-    return symbols[Math.floor(Math.random() * symbols.length)];
-
-}
-// console.log(symbols());
+console.log(password)
